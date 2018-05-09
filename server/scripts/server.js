86a97e9b9a437c96197c927a30909c620623b041
oliegjio <oliegjio@gmail.com>
2018-05-09 16:38:02 +0300

    Added some server code

--- server/scripts/server.js
+++ server/scripts/server.js
@@ -2,10 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var net_1 = require("net");
 var server = net_1.createServer(function (socket) {
-    socket.write('Server started.\n');
+    // Send to the client:
+    // socket.write('You are connected')
+    // Get data from the client:
+    socket.on('data', function (data) {
+        console.log(data);
+    });
+    // Closing connection with the client:
+    socket.on('end', function () {
+        console.log('Closing connection');
+    });
     socket.pipe(socket);
 });
 server.on('connection', function (socket) {
-    console.log('New connection.');
+    console.log('New connection');
 });
 server.listen(1337, 'localhost');
--- server/scripts/server.ts
+++ server/scripts/server.ts
@@ -1,12 +1,25 @@
 import { createServer, Server, Socket } from 'net'
 
 let server: Server = createServer((socket: Socket) => {
-    socket.write('Server started.\n')
+
+    // Send to the client:
+    // socket.write('You are connected')
+
+    // Get data from the client:
+    socket.on('data', (data) => {
+        console.log(data)
+    })
+
+    // Closing connection with the client:
+    socket.on('end', () => {
+        console.log('Closing connection')
+    })
+
     socket.pipe(socket)
 })
 
 server.on('connection', (socket: Socket) => {
-    console.log('New connection.')
+    console.log('New connection')
 })
 
 server.listen(1337, 'localhost')
